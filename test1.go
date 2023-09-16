c69153d89ab3bcd26c7ea1c5c99b409acbf9cad3
HoangQuocLuat <2021050769@student.humg.edu.vn>
2023-09-16 10:51:55 +0700

    completed baitap

--- test1.go
+++ test1.go
@@ -3,21 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{20, 5 , 10, 3}
+	a := []int{20, 20, 10, 4, 15}	
+	max1 := a[0]
+	max2 := 0
 	if len(a) < 2 {
 		return
 	}
-	max1 := a[0]
-	max2 := 0
 	for _, value := range a {
 		if  value > max1 {
 			max2 = max1
 			max1 = value
 			value = max2
-		} else if (value < max1 ) {
-			max2 = max1
-			max1 = value
-			value = max2 
+			fmt.Println("check",max2, value, max1)
+			
+		} else if (value < max1 && value > max2 ) {
+			fmt.Println("check",max2, value, max1)
+			max2 = value
 		}
 
 	}
